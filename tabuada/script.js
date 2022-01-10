dc16fb6d3ba5491b358580bfb77f4e3fdc30ded6
Edilaine Alves dos Santos <edilaine.santos839@gmail.com>
2022-01-10 16:16:30 -0300

    finalizei-tabuada

--- tabuada/script.js
+++ tabuada/script.js
@@ -5,6 +5,17 @@ function exibir(){
         alert('Por favor, informe um número!')
     }
     else{
+        var tab = document.querySelector('#tab')
+
         numero = Number(numero)
+
+        tab.innerHTML = ''
+
+        for(var i=0; i<=10; i++){
+            var item = document.createElement('option')
+            item.text = `${numero}x${i} = ${numero*i}`
+            item.value = `tab${i}` //para outras linguagens
+            tab.appendChild(item)
+        }
     }
 }
