7761cf8662832e1e8e54f2dde0b1d14cf38f0e02
Edilaine Alves dos Santos <edilaine.santos839@gmail.com>
2022-01-29 15:44:10 -0300

    correcao-erro-edit

--- crud simples/main.js
+++ crud simples/main.js
@@ -3,32 +3,30 @@
 /*********************************************************************************************/
 "use strict";
 
-const openModal = () =>
-  document.getElementById("modal").classList.add("active");
+let openModal = () => document.getElementById("modal").classList.add("active");
 
-const closeModal = () => {
+let closeModal = () => {
   clearFields();
   document.getElementById("modal").classList.remove("active");
 };
 
-const closeModalButton = () => {
+let closeModalButton = () => {
   clearFields();
   document.getElementById("modal").classList.remove("active");
 };
 
-const getLocalStorage = () =>
-  JSON.parse(localStorage.getItem("db_client")) ?? [];
+let getLocalStorage = () => JSON.parse(localStorage.getItem("db_client")) ?? [];
 //get recebe
 //nessa função, eu estou recebendo, e transformando o que está sendo recebido em um objeto
 //?? [] significa: se isso não existir, me retorna um array vazio
 
-const setLocalStorage = (dbClient) =>
+let setLocalStorage = (dbClient) =>
   localStorage.setItem("db_client", JSON.stringify(dbClient));
 //set envia
 //eu envio uma key (db_client) e um value (client)
 //o JSON.stingfy vai transformar meu objeto em um valor
 
-const isValidFields = () => {
+let isValidFields = () => {
   return document.getElementById("form").reportValidity(); //retornando se campos foram preenchidos ou não
 };
 
@@ -36,9 +34,9 @@ const isValidFields = () => {
 /*CREATE - FUNÇÃO*****************************************************************************/
 /*********************************************************************************************/
 
-const createClient = (client) => {
-  /*const db_client = [] //declarando como array (não funciona pois o dado é substituído)*/
-  const dbClient = getLocalStorage();
+let createClient = (client) => {
+  /*let db_client = [] //declarando como array (não funciona pois o dado é substituído)*/
+  let dbClient = getLocalStorage();
   dbClient.push(client); //adiona novos itens no final do array
 
   setLocalStorage(dbClient);
@@ -47,13 +45,13 @@ const createClient = (client) => {
 /*********************************************************************************************/
 /*READ - FUNÇÃO*******************************************************************************/
 /*********************************************************************************************/
-const readClient = () => getLocalStorage();
+let readClient = () => getLocalStorage();
 
 /*********************************************************************************************/
 /*UPDATE - FUNÇÃO*****************************************************************************/
 /*********************************************************************************************/
-const updateClient = (index, client) => {
-  const dbClient = readClient();
+let updateClient = (index, client) => {
+  let dbClient = readClient();
   dbClient[index] = client; //posição recebe novo cliente
   setLocalStorage(dbClient);
 }; //essa função recebe meu index (id vai de 0 a ...) como parametro pra saber quem editar
@@ -61,8 +59,8 @@ const updateClient = (index, client) => {
 /*********************************************************************************************/
 /*DELETE - FUNÇÃO*****************************************************************************/
 /*********************************************************************************************/
-const deleteClient = (index) => {
-  const dbClient = readClient();
+let deleteClient = (index) => {
+  let dbClient = readClient();
   dbClient.splice(index, 1); //1 é de avançar uma casa, se eu colocar 2, ele vai excluir o index e o próximo; mas se eu colocar 0, eu poderia adionar um elemento (mas pra essa função não faz sentido)
   setLocalStorage(dbClient);
 };
@@ -74,26 +72,27 @@ const deleteClient = (index) => {
 /******************************/
 /*LIMPEZA**********************/
 /******************************/
-const clearFields = () => {
-  const fields = document.querySelectorAll(".modal-field"); //pegando os campos do meu formulario com essa classe
+let clearFields = () => {
+  let fields = document.querySelectorAll(".modal-field"); //pegando os campos do meu formulario com essa classe
   fields.forEach((field) => (field.value = "")); //para cada campo de fields, eu crio uma variavel chamada field, e ela recebe null
+  document.getElementById("nome").dataset.index = "new";
 };
 
 /******************************/
 /*SALVAR***********************/
 /******************************/
 
-const saveClient = () => {
+let saveClient = () => {
   if (isValidFields()) {
     //só cadastra se a função voltar como verdadeira
-    const client = {
-      //constroi novo cliente
+    let client = {
+      //letroi novo cliente
       nome: document.getElementById("nome").value,
       email: document.getElementById("email").value,
       celular: document.getElementById("cel").value,
       cidade: document.getElementById("cidade").value,
     };
-    const index = document.getElementById("nome").dataset.index;
+    let index = document.getElementById("nome").dataset.index;
     if (index == "new") {
       createClient(client); //envia pro localstorage com função que criei
       updateTable();
@@ -109,9 +108,9 @@ const saveClient = () => {
 /******************************/
 /*ADICIONANDO EM HTML**********/
 /******************************/
-const createRow = (client, index) => {
-  const newRow = document.createElement("tr");
-  newRow.innerHTML = `<td>${client.nome}</td>
+let createRow = (client, index) => {
+  let newRow = document.createElement("tr");
+  newRow.innerHTML += `<td>${client.nome}</td>
                     <td>${client.email}</td>
                     <td>${client.celular}</td>
                     <td>${client.cidade}</td>
@@ -125,12 +124,12 @@ const createRow = (client, index) => {
   document.querySelector("#tableClient>tbody").appendChild(newRow);
 };
 
-const clearTable = () => {
-  const rows = document.querySelectorAll("#tableClient>tbody tr"); //pego as tr dentro de tbody que tá dentro de tableClient
+let clearTable = () => {
+  let rows = document.querySelectorAll("#tableClient>tbody tr"); //pego as tr dentro de tbody que tá dentro de tableClient
   rows.forEach((row) => row.parentNode.removeChild(row)); //pega cada linha, pega o elemento pai dela, que é o tbody, e apaga o filho
 };
-const updateTable = () => {
-  const dbClient = readClient(); //lê clientes do banco
+let updateTable = () => {
+  let dbClient = readClient(); //lê clientes do banco
   clearTable(); //função necessária, pois cada vez que eu faço um update, as informações vem replicadas
   dbClient.forEach(createRow); //forEach pega cada cliente e envia pra minha função createRow
   //o forEach envia 3 informações (o elemento do array, o index(indice),)
@@ -139,7 +138,7 @@ const updateTable = () => {
 /******************************/
 /*EDITAR E REMOVER*************/
 /******************************/
-const fillFields = (client) => {
+let fillFields = (client) => {
   document.getElementById("nome").value = client.nome;
   document.getElementById("email").value = client.email;
   document.getElementById("cel").value = client.celular;
@@ -147,17 +146,17 @@ const fillFields = (client) => {
   document.getElementById("nome").dataset.index = client.index;
 };
 
-const editClient = (index) => {
-  const client = readClient()[index]; //recebe o array de posição tal
+let editClient = (index) => {
+  let client = readClient()[index]; //recebe o array de posição tal
   client.index = index;
   fillFields(client);
   openModal();
 };
 
-const editDelete = (event) => {
+let editDelete = (event) => {
   if (event.target.type == "button") {
     //o target retorna onde foi clicado
-    const [action, index] = event.target.dataset.action.split("-");
+    let [action, index] = event.target.dataset.action.split("-");
     //primeiro eu crio um array que pega a ação e a posição
     //depois tô pegando meu atributo personalizado de nome action
     //o split vai separar minha ação da posição pelo '-'
@@ -165,10 +164,8 @@ const editDelete = (event) => {
     if (action == "editar") {
       editClient(index);
     } else {
-      const client = readClient()[index];
-      const response = confirm(
-        `Deseja mesmo excluir o cliente ${client.nome}?`
-      );
+      let client = readClient()[index];
+      let response = confirm(`Deseja mesmo excluir o cliente ${client.nome}?`);
       if (response) {
         deleteClient(index);
         updateTable();
@@ -183,18 +180,18 @@ updateTable();
 /*BUSCAR***********************/
 /******************************/
 
-const searchClient = () => {
-  const busca = document.getElementById("txtSearch").value.toLowerCase();
+let searchClient = () => {
+  let busca = document.getElementById("txtSearch").value.toLowerCase();
 
   for (let i = 0; i < tbody.children.length; i++) {
     let achou = false; //tipo let pra ser reatribuído depois
-    const tr = tbody.children[i];
-    const td = tr.children;
+    let tr = tbody.children[i];
+    let td = tr.children;
     //childNodes não deu certo, pois ele pegou elementos text nodes e comentários nodes
     //children retorna apenas elementos
-    
-    for (let j = 0; j < td.length-1; j++) {
-      const value = td[j].childNodes[0].nodeValue.toLowerCase();
+
+    for (let j = 0; j < td.length - 1; j++) {
+      let value = td[j].childNodes[0].nodeValue.toLowerCase();
       if (value.indexOf(busca) >= 0) {
         //indexOf procura uma ocorrencia
         achou = true;
@@ -212,8 +209,7 @@ const searchClient = () => {
 /*********************************************************************************************/
 /*EVENTOS*************************************************************************************/
 /*********************************************************************************************/
-document
-  .getElementById("txtSearch").addEventListener("keyup", searchClient)
+document.getElementById("txtSearch").addEventListener("keyup", searchClient);
 
 document
   .getElementById("cadastrarCliente")
